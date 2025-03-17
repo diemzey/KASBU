b95069559ee799204014f54034109c098b595064
victor <vjbsh@hotmail.com>
2025-03-17 11:08:36 -0600

    specify the full callback url

--- src/utils/auth-client.ts
+++ src/utils/auth-client.ts
@@ -12,7 +12,7 @@ export const googleSignIn = async (username: string) => {
   const { data, error } = await authClient.signIn.social(
     {
       provider: "google",
-      callbackURL: "kasbu.com/app",
+      callbackURL: "https:/kasbu.com/app",
     },
     {
       onSuccess: async (ctx) => {
